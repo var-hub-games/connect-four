8bb7f945c2238e235eb35eaead00b8ec3cd9d5e5
dpohvar <dpohvar@gmail.com>
2024-11-11 02:35:12 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import varhubBundlePlugin from "@flinbein/rollup-plugin-varhub-bundle";
 
 export default {
 	root: "./src",
+	build: {
+		outDir: "../dist",
+		emptyOutDir: true,
+	},
 	plugins: [varhubBundlePlugin()],
 } satisfies UserConfig
